70c673a744524fff82b376e7099b8ef305d2d406
Lee Meyer <lee.meyer@gmail.com>
2020-10-31 16:25:30 +1100

    vuex the things

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,4 @@
 <template>
-  <div class="hello">
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  <div>
-
     <beautiful-chat ref="chat"
       :participants="participants"
       :titleImageUrl="titleImageUrl"
@@ -50,7 +17,7 @@
       :showLauncher="true"
       :showCloseButton="true"
       :colors="colors"
-      :alwaysScrollToBottom="alwaysScrollToBottom"
+      :alwaysScrollToBottom="true"
       :disableUserListToggle="false"
       :messageStyling="messageStyling"  
       @edit="editMessage" >
@@ -67,14 +34,13 @@
         </div>
     </template>
     </beautiful-chat>
-  </div>
-  </div>
 </template>
 
 <script>
 import useRhymeHighlight from './use-rhyme-highlight';
 import TransitionedWords from './TransitionedWords'
 import AnimatedNumber from './AnimatedNumber'
+import { mapState } from 'vuex';
 
 export default {
   components: {
@@ -84,30 +50,11 @@ export default {
   name: 'app',
   setup() {     
       return useRhymeHighlight(); 
-
   },
   data() {
     return {
-      participants: [
-        {
-          id: 'user1',
-          name: 'Matteo',
-          imageUrl: 'https://avatars3.githubusercontent.com/u/1915989?s=230&v=4'
-        },
-        {
-          id: 'me',
-          name: 'Support',
-          imageUrl: 'https://avatars3.githubusercontent.com/u/37018832?s=200&v=4'
-        }
-      ], // the list of all the participant of the conversation. `name` is the user name, `id` is used to establish the author of a message, `imageUrl` is supposed to be the user avatar.
       titleImageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png',
-      messageList: [
-          { id: 1, type: 'text', author: `me`, data: { text: `Say yes!` }, score: 65 },
-          { id: 2, type: 'text', author: `user1`, data: { text: `No.` }, score: 65 }
-      ], // the list of the messages to show, can be paginated and adjusted dynamically
-      newMessagesCount: 0,
-      isChatOpen: false, // to determine whether the chat window should be open or closed
-      showTypingIndicator: '', // when set to a value matching the participant.id it shows the typing indicator for the specific user
+      showTypingIndicator: '', 
       colors: {
         header: {
           bg: '#4e8cff',
@@ -131,55 +78,35 @@ export default {
           bg: '#f4f7f9',
           text: '#565867'
         }
-      }, // specifies the color scheme for the component
-      alwaysScrollToBottom: true, // when set to true always scrolls the chat to the bottom when new events are in (new message, user starts typing...)
-      messageStyling: true // enables *bold* /emph/ _underline_ and such (more info at github.com/mattezza/msgdown)
+      }, 
+      alwaysScrollToBottom: true, 
+      messageStyling: true 
     }
   },
+  computed: mapState({
+    messageList: state => state.messageList,
+    isChatOpen: state => state.isChatOpen,
+    newMessagesCount: state => state.newMessagesCount,
+    participants: state => state.participants
+  }),
   methods: {
-    afterEnter() {
-      console.log('please!')
-    },
     sendMessage (text) {
-      if (text.length > 0) {
-        this.newMessagesCount = this.isChatOpen ? this.newMessagesCount : this.newMessagesCount + 1
-        this.onMessageWasSent({ author: 'me', type: 'text', data: { text } })
-      }
+      this.$store.commit('sendMessage', text);
     },
     onMessageWasSent (message) {
-      this.messageList = [ ...this.messageList, message ]
+      this.$store.commit('onMessageWasSent', message);
     },
     openChat () {
-      this.isChatOpen = true
-      this.newMessagesCount = 0;
-
-      let latest = this.messageList; 
-      
-      if (latest.length > 10)
-      {
-        latest = this.messageList.slice(1).slice(-10)
-      }
-
-      latest.forEach((m, i) => m.animatedScoreDelay = `${i * .5}s`);
-
+      this.$store.commit('openChatWindow');
     },
     closeChat () {
-      this.isChatOpen = false
-    },
-    handleScrollToTop () {
-      // called when the user scrolls message list to top
-      // leverage pagination for loading another page of messages
-    },
-    handleOnType() {
+     this.$store.commit('closeChatWindow');
     },
     editMessage(message){
-      const m = this.messageList.find(m=>m.id === message.id);
-      m.isEdited = true;
-      m.data.text = message.data.text;
-      m.score = 102;      
+      this.$store.commit('editMessage', message);
     },
     clickScore(message) {
-      message.data.text = 'yes boss Say like a';
+      this.$store.commit('explainScore', message);
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,59 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    isChatOpen: false,
+    messageList: [
+      { id: 1, type: 'text', author: `me`, data: { text: `Say yes!` }, score: 65, scoreAnimatedEntranceDelay : 0  },
+      { id: 2, type: 'text', author: `user1`, data: { text: `No.` }, score: 65, scoreAnimatedEntranceDelay : 0  }
+    ],
+    participants: [
+      {
+        id: 'user1',
+        name: 'Matteo',
+        imageUrl: 'https://avatars3.githubusercontent.com/u/1915989?s=230&v=4'
+      },
+      {
+        id: 'me',
+        name: 'Support',
+        imageUrl: 'https://avatars3.githubusercontent.com/u/37018832?s=200&v=4'
+      }
+    ]
   },
   mutations: {
-  },
-  actions: {
-  },
-  modules: {
+    openChatWindow(state) {
+      
+      state.isChatOpen = true
+      state.newMessagesCount = 0;
+
+      let latest = state.messageList; 
+      
+      if (latest.length > 10)
+      {
+        latest = state.messageList.slice(1).slice(-10)
+      }
+
+      latest.forEach((m, i) => m.scoreAnimatedEntranceDelay = `${i * .5}s`);
+    },
+    closeChatWindow(state) {
+      state.isChatOpen = false;
+    },
+    sendMessage(state, text) {
+      if (text.length > 0) {
+        state.newMessagesCount = state.isChatOpen ? state.newMessagesCount : state.newMessagesCount + 1;
+        this.commit('onMessageWasSent', { author: 'me', type: 'text', data: { text } });
+      }
+    },
+    onMessageWasSent(state, message) {
+      state.messageList = [ ...state.messageList, message ]
+    },
+    editMessage(state, message){
+      const m = state.messageList.find(m=>m.id === message.id);
+      m.isEdited = true;
+      m.data.text = message.data.text;
+      m.score += 102;
+    },
+    explainScore(state, message) {
+      message.data.text = 'yes boss Say like a';
+    }
   }
 })
